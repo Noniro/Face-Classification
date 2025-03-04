d9006dbb4d8c189663122c9397a27f99092394bb
noniro <yuvalv126@gmail.com>
2025-03-04 15:36:03 +0200

    update

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,8 +2,24 @@ import os
 import cv2
 import numpy as np
 
+"""
+From the kuggle fwl data set "The original images are 250 x 250 pixels,
+but the default slice and resize arguments reduce them to 62 x 47 pixels.
+"""
 
 def load_data(dataset_path, img_size=(62, 47)):
+    """
+    Load images and labels from the dataset.
+
+    Parameters:
+    - dataset_path: str, path to the dataset directory.
+    - img_size: tuple, size to which each image will be resized (width, height).
+
+    Returns:
+    - X: np.ndarray, array of images normalized to [0, 1].
+    - y: np.ndarray, array of labels corresponding to the images.
+    - label_dict: dict, mapping from label indices to person names.
+    """
     images, labels, label_dict = [], [], {}
     person_classes = sorted(os.listdir(dataset_path))
 
@@ -23,7 +39,7 @@ def load_data(dataset_path, img_size=(62, 47)):
                     images.append(img)
                     labels.append(label_idx)
 
-    X = np.array(images, dtype="float32") / 255.0
-    y = np.array(labels)
+    X = np.array(images, dtype="float32") / 255.0  # Normalize images to [0, 1]
+    y = np.array(labels)  # Array of labels , number for each person
 
     return X, y, label_dict
--- utils/data_splitter.py
+++ utils/data_splitter.py
@@ -1,12 +1,16 @@
 from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
+import numpy as np
 
-
-
+# Splitting the data into training and testing sets.
 def split_data(X, y, test_size=0.2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42, stratify=y)
-    num_classes = len(set(y))
-    y_train = to_categorical(y_train, num_classes)
-    y_test = to_categorical(y_test, num_classes)
 
-    return X_train, X_test, y_train, y_test
+    num_classes = len(set(y))
+    y_train_cat = np.zeros((y_train.size, num_classes))
+    y_train_cat[np.arange(y_train.size), y_train] = 1
+
+    y_test_cat = np.zeros((y_test.size, num_classes))
+    y_test_cat[np.arange(y_test.size), y_test] = 1
+
+    return X_train, X_test, y_train_cat, y_test_cat
+# y_train_cat and y_test_cat - [num_samples, num_classes] one-hot encoded labels (each row is the class label for a sample).
